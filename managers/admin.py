45e019ee72a7277ce1d057b1c414284decf696af
Zgalis9435 <zharos.delgado@proton.me>
2023-02-25 22:21:13 +0100

    Final commit

--- managers/admin.py
+++ managers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import genere,nameManager,movieTitle,summary
+
+admin.site.register(genere)
+admin.site.register(nameManager)
+admin.site.register(movieTitle)
+admin.site.register(summary)
--- managers/models.py
+++ managers/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class nameManager(models.Model):
+    name=models.CharField(max_length=64,help_text='Introduce el nombre del director')
+
+    def __str__(self):
+        return self.name
+
+
+class genere(models.Model):
+    genereName=models.CharField(max_length=64,help_text='Introduce el genero de la pelicula')
+
+class summary(models.Model):
+    summaryResume=models.TextField(max_length=300,help_text='Introduce un resumen de la pelicula')
+
+
+
+class movieTitle(models.Model):
+    movieName=models.CharField(max_length=64,help_text='Introduce el titulo de la pelicula')
+    nameManager=models.ForeignKey(nameManager, on_delete=models.SET_NULL, null=True)
+    summary=models.ForeignKey(summary, on_delete=models.SET_NULL, null=True)
+    genere=models.ManyToManyField(genere)
+
+    def __str__(self):
+        return self.movieName
